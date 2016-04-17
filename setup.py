12e5d14cd4507539e6071f62eaaa5b9d6bdc8fca
clach04 <Chris.Clark@actian.com>
2016-04-16 20:22:49 -0700

    Import of passogva 1.0 dated 2010-03-10
    From https://pypi.python.org/pypi/passogva
    
    A FIPS-181 Automated Password Generator implementation.
    Generates random, pronounceable words.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="passogva",
+    version="1.0",
+    description="A FIPS-181 Automated Password Generator implementation. Generates random, pronounceable words.",
+    author="Steven Hazel",
+    author_email="sah@awesame.org",
+    maintainer="Steven Hazel",
+    maintainer_email="sah@awesame.org",
+    py_modules=['passogva']
+)
