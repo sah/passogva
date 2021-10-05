205c5a02d18ff74cf4fa741645cc58db29a8a042
Steven Hazel <sah@stevenhazel.org>
2021-10-05 00:28:23 -0700

    Clarifying license and preparting for version 1.1.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # passogva
 
-Import and few tweaks of https://pypi.python.org/pypi/passogva
-original website is offline as of April 2016.
-
 A FIPS-181 Automated Password Generator implementation. Generates random,
 pronounceable words.
 
--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import setup
 
 setup(
     name="passogva",
-    version="1.0",
+    version="1.1",
     description="A FIPS-181 Automated Password Generator implementation. Generates random, pronounceable words.",
     author="Steven Hazel",
     author_email="sah@awesame.org",
     maintainer="Steven Hazel",
     maintainer_email="sah@awesame.org",
-    py_modules=['passogva']
+    py_modules=['passogva'],
+    license="MIT",
+        classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    platforms=["any"],
 )
